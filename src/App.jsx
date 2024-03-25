8a7518fc49f7e878657109be1d26d9bee67aa55a
Nedhen <nedheng.com>
2024-03-25 23:27:40 +0530

    added card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import RevenueCard from "./components/RevenueCard"
+import BlueCard from "./components/BlueCard"
+
 
 function App() {
-  
-  return <>
-   <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  </>
+
+  return (
+    <div>
+      <div>hi</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                
+                <BlueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={13} />
+                <RevenueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={13} />
+                <RevenueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={13} />
+                
+            </div>
+
+    </div>
+  )
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue:{
+          700: "#146EB4",
+          750: "#1174c2",
+          800: "#1E2640",
+          900: "#0E4F82"
+        }
+      },
+      boxShadow: {
+        'custom': '0 2px 6px 0px rgba(0, 0, 0, 0.2)',
+      }
+    },
   },
   plugins: [],
 }
